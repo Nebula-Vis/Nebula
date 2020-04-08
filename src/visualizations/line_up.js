d9c7de6835fe763bcde5c29939ab571b4b25b95d
Jiahui Chen <jhchen6@outlook.com>
2020-04-08 13:36:38 +0800

    fix(lineup): add filter reactivity

--- src/visualizations/line_up.js
+++ src/visualizations/line_up.js
@@ -16,6 +16,8 @@ export default class LineUp {
     this.lineup = null
     this._selection = null
     this._order = null
+    this._data = null
+
     this.colors = d3.scaleOrdinal(d3.schemeSet2)
 
     this._init()
@@ -23,40 +25,23 @@ export default class LineUp {
 
   _init() {
     this._buildLineUp()
-    this.data = new ReactiveProperty(this, 'data', this.data, '_onDataChange')
+    this.data = new ReactiveProperty(this, 'data', this.data, '_onDataSet')
     this.selection = new ReactiveProperty(
       this,
       'selection',
       this.selection,
-      '_onSelectionChange'
+      '_onSelectionSet'
     )
     this.order = new ReactiveProperty(
       this,
       'order',
       this.order,
-      '_onOrderChange'
+      '_onOrderSet'
     )
 
-    this.lineup.on('selectionChanged', (selectedIndices) => {
-      const data = this.data.value
-      const nbids = selectedIndices
-        .filter((index) => !!data[index])
-        .map((index) => data[index]._nbid_)
-      this._selection = nbids
-      this.selection.set(nbids)
-    })
-
-    this.lineup.data.getFirstRanking().on(
-      'moveColumn',
-      _.debounce(() => {
-        const order = this.lineup.data
-          .getFirstRanking()
-          .children.slice(4)
-          .map((c) => c.desc.column)
-        this._order = order
-        this.order.set(order)
-      }, 50)
-    )
+    this._addDataListener()
+    this._addSelectionListener()
+    this._addOrderListener()
   }
 
   _buildLineUp() {
@@ -69,7 +54,19 @@ export default class LineUp {
     this.lineup = builder.build(d3.select(this.el).node())
   }
 
-  _onDataChange(data) {
+  _getDataBuilder(data, order) {
+    const builder = LineUpJS.builder(data)
+
+    builder.column(LineUpJS.buildStringColumn('_nbid_').width(150))
+    order.forEach((attr) => {
+      builder.column(LineUpJS.buildNumberColumn(attr).color(this.colors(attr)))
+    })
+
+    return builder
+  }
+
+  _onDataSet(data) {
+    if (_.isEqual(data, this._data)) return
     const datum = data[0]
     const order = this.order.value.filter((attr) => datum.hasOwnProperty(attr))
     const builder = this._getDataBuilder(data, order)
@@ -79,9 +76,11 @@ export default class LineUp {
         data.findIndex((v) => v._nbid_ === nbid)
       )
     )
+    this._addDataListener()
+    this._addOrderListener()
   }
 
-  _onSelectionChange(selection) {
+  _onSelectionSet(selection) {
     if (_.isEqual(selection, this._selection)) return
     const data = this.data.value
     const selectedIndices = selection.map((nbid) =>
@@ -91,7 +90,7 @@ export default class LineUp {
     this.lineup.setSelection(selectedIndices)
   }
 
-  _onOrderChange(order) {
+  _onOrderSet(order) {
     if (_.isEqual(order, this._order)) return
     const ranking = this.lineup.data.getFirstRanking()
     const columns = ranking.children
@@ -103,14 +102,45 @@ export default class LineUp {
       })
   }
 
-  _getDataBuilder(data, order) {
-    const builder = LineUpJS.builder(data)
-
-    builder.column(LineUpJS.buildStringColumn('_nbid_').width(150))
-    order.forEach((attr) => {
-      builder.column(LineUpJS.buildNumberColumn(attr).color(this.colors(attr)))
+  _addDataListener() {
+    const lineupData = this.lineup.data
+    const ranking = lineupData.getFirstRanking()
+    let isNewlyFiltered = false
+    ranking.on('filterChanged', () => {
+      isNewlyFiltered = true
     })
 
-    return builder
+    lineupData.on('orderChanged', (prev, cur) => {
+      if (!isNewlyFiltered) return
+      isNewlyFiltered = false
+      const data = this.data.value.filter((d, i) => cur.includes(i))
+      this._data = data
+      this.data.set(data)
+    })
+  }
+
+  _addSelectionListener() {
+    this.lineup.on('selectionChanged', (selectedIndices) => {
+      const data = this.data.value
+      const nbids = selectedIndices
+        .filter((index) => !!data[index])
+        .map((index) => data[index]._nbid_)
+      this._selection = nbids
+      this.selection.set(nbids)
+    })
+  }
+
+  _addOrderListener() {
+    this.lineup.data.getFirstRanking().on(
+      'moveColumn',
+      _.debounce(() => {
+        const order = this.lineup.data
+          .getFirstRanking()
+          .children.slice(4)
+          .map((c) => c.desc.column)
+        this._order = order
+        this.order.set(order)
+      }, 50)
+    )
   }
 }
